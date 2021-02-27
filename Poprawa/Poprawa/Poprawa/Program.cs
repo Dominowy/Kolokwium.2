547d63d154fdec9269825ac908315851e30bd398
Dominik Katana <hejdominowy@outlook.com>
2021-02-27 09:13:01 +0100

    1

---
+++ Poprawa/Poprawa/Poprawa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Poprawa
+{
+    public class Ankieta
+    {
+        
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
